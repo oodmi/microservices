6891ad0a4d829341dda679849c3bfbde097aaf06
dmitry <dmitry.o.ovchinnikov@gmail.com>
2018-11-14 19:55:58 +0300

    Use response header

--- gateway/src/main/java/com/oodmi/registry/controller/MainController.java
+++ gateway/src/main/java/com/oodmi/registry/controller/MainController.java
@@ -1,16 +1,17 @@
 package com.oodmi.registry.controller;
 
+import static com.oodmi.security.SecurityConstants.HEADER_STRING;
+
 import com.oodmi.registry.client.AuthClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import static com.oodmi.security.SecurityConstants.HEADER_STRING;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,13 +23,14 @@ public class MainController {
     private final HttpServletResponse res;
 
     @GetMapping(value = "")
-    public String index(@RequestParam(value = "code", required = false) String code) throws Exception {
-
+    public String index(Model model, @RequestParam(value = "code", required = false) String code) throws Exception {
         if (code != null) {
             log.info("vk login");
             String token = authClient.vk(code, req.getRequestURL().toString().substring(0, req.getRequestURL().toString().length() - 1));
             res.addHeader(HEADER_STRING, token);
-
+            model.addAttribute("token", token);
+        } else {
+            model.addAttribute("token", "");
         }
         return "main/index";
     }
--- gateway/src/main/resources/templates/main/index.ftl
+++ gateway/src/main/resources/templates/main/index.ftl
@@ -2,6 +2,7 @@
 <#include "/partials/header.ftl">
  <div class="row">
      <button id="vk" class="waves-effect waves-light btn">Vk</button>
+     <div id="token" hidden>${token}</div>
  </div>
 
 <script>
@@ -17,5 +18,12 @@
                 "&response_type=code" +
                 "&v=5.87"
     });
+    $(function () {
+        var el = $("#token");
+        console.log(el.text());
+        localStorage.setItem("token", el.text());
+        el.text('');
+    });
+
 </script>
 <#include "/partials/footer.ftl">
