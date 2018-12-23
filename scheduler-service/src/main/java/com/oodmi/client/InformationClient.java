f458a2f764eb89976b4aafe12dfab03bcf1c5b11
Dmitry Ovchinnikov <dmitry.o.ovchinnikov@gmail.com>
2018-12-23 10:45:48 +0300

    fix scheduler feign client

--- scheduler-service/src/main/java/com/oodmi/client/InformationClient.java
+++ scheduler-service/src/main/java/com/oodmi/client/InformationClient.java
@@ -4,11 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("information-service")
 public interface InformationClient {
 
-    @GetMapping(value = "update/{login}", consumes = "application/json")
+    @GetMapping(value = "friend/update/{login}", consumes = "application/json")
     ResponseEntity update(@PathVariable("login") String login);
 }
